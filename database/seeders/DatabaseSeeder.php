85633a558f604c19a4aae662b78ae67865b49516
Your Name <you@example.com>
2026-04-12 03:37:14 +0500

    making it work

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,11 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminUserSeeder::class,
-            WalletCurrencySeeder::class,
-            BankAccountSeeder::class,
-            BillPaymentCategorySeeder::class,
-            BillPaymentProviderSeeder::class,
-            BillPaymentPlanSeeder::class,
             PlatformRateSeeder::class,
         ]);
     }
